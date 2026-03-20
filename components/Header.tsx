104279baa1c8939d2c34953bbd466dc1f2c6670e
Jorge-6969 <jorgeosornioplancarte@gmail.com>
2026-03-20 12:49:14 -0600

    Boton para vizualizar productos.

--- components/Header.tsx
+++ components/Header.tsx
@@ -51,7 +51,17 @@ export default function Header() {
                         >
                             👤 Perfil
                         </Link>
+                         
+                        <Link
+                            href= "products/list"
+                            className="block px-4 py-2 hover:bg-gray-100"
+                            onClick={() => setOpen(false)}
+                        >
+                           🍎  Productos
+                        </Link>
                     </div>
+
+
                 )}
             </div>
         </header>
