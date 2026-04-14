1d20bde86c3d5c69f0726a8abcf37d784ee5811c
Javier <javrmndza@gmail.com>
2026-04-14 14:01:25 -0600

    cambios de botones

--- components/Header.tsx
+++ components/Header.tsx
@@ -53,7 +53,7 @@ export default function Header() {
                         </Link>
                          
                         <Link
-                            href= "products/list"
+                            href= "/products/list"
                             className="block px-4 py-2 hover:bg-gray-100"
                             onClick={() => setOpen(false)}
                         >
